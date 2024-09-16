228fec9b3b3ad8b021753d01a47a5d5fa41304aa
Pulkit Porwal <pulkitporwal.brn@gmail.com>
2024-09-16 19:52:48 +0530

    Access-Control-Allow-Origin Header has been added to Singup File

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -34,6 +34,7 @@ const Signup = () => {
       {
         method: "POST",
         headers: {
+          "Access-Control-Allow-Origin":"*",
           "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
